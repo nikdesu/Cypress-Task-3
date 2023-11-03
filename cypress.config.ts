8115b6b2a5aefe1813b09aa45b9bb3a0b16f6dfc
nikdesu <ostspchyk1409@gmail.com>
2023-11-03 22:00:44 +0200

    config changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'wpbsxw',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
