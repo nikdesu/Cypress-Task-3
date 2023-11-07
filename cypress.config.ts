3d9ffd919029198c796b2f940a67bbffbaec5579
nikdesu <ostspchyk1409@gmail.com>
2023-11-07 14:46:52 +0200

    added html reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "wpbsxw",
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://demo.realworld.io/#/",
+    setupNodeEvents(on, config) {
+
+            require('cypress-mochawesome-reporter/plugin')(on);
+      
+          },
   },
 });
